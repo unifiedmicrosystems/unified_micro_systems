11f2ff43942e8bea64ebfa2e64ba6085e1afbc9d
nitish <developer.scorpio@gmail.com>
2020-02-19 13:29:43 +0530

    updated technology partners

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,7 +6,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-article-template-js": hot(preferDefault(require("/opt/lampp/htdocs/unifiedmicro_systems/src/templates/articleTemplate.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/opt/lampp/htdocs/unifiedmicro_systems/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/opt/lampp/htdocs/unifiedmicro_systems/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/opt/lampp/htdocs/unifiedmicro_systems/src/pages/index.js"))),
   "component---src-pages-our-story-js": hot(preferDefault(require("/opt/lampp/htdocs/unifiedmicro_systems/src/pages/our-story.js"))),
