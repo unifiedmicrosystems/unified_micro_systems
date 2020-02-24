7519a10b8efe5c7374014e73f0690d5421560e1c
nitish <developer.scorpio@gmail.com>
2020-02-24 17:36:07 +0530

    article posts

--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -6,11 +6,16 @@ export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+    {
+      data.allMarkdownRemark.edges.map(({ node }) => (
       <div>
       <img fluid={node.frontmatter.featuredImage.childImageSharp.fluid.src} className="attachment-full" alt="strategy" />
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
+      )
+      )
+    }
     </Layout>
   )
 }
