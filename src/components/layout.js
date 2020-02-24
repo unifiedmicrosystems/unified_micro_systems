78ee4a675a07e906f58f3e970127da3caa878a0c
nitish <developer.scorpio@gmail.com>
2020-02-24 18:38:17 +0530

    mobile menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -183,70 +183,70 @@ return (
                     <div className="mobile-menu"> 
                         <ul className="menu-main-menu">
                           <li className="menu-item">
-                            <Link to="javascript:void(0)" className=" current "><span>Home</span></Link>
+                            <Link to="/" className=" current "><span>Home</span></Link>
                           </li>
                           <li className="menu-item has-sub-menu open_sub">
-                            <Link to="javascript:void(0)" className="current"><span>About</span></Link>
+                            <Link to="#" className="current"><span>About</span></Link>
                             <ul className="sub_menu">
                               <li className="menu-item">
-                                <Link to="our-story.html" className=""><span>Our Story</span></Link>
+                                <Link to="/our_story" className=""><span>Our Story</span></Link>
                               </li>
                               <li className="menu-item">
-                                <Link to="technology-partners.html" className=""><span>Technology Partners</span></Link>
+                                <Link to="/technology_partners" className=""><span>Technology Partners</span></Link>
                               </li>
                               <li className="menu-item">
-                                <Link to="resellers-business-partners.html" className="">
+                                <Link to="/resellers_business_partners" className="">
                                   <span>Resellers &amp; Business Partners</span>
                                 </Link>
                               </li>
                               <li className="menu-item">
-                                <Link to="terms-and-conditions.html" className=""><span>Terms and Conditions</span></Link>
+                                <Link to="/terms_and_conditions" className=""><span>Terms and Conditions</span></Link>
                               </li>
                               <li className="menu-item">
-                                <Link to="careers.html" className=""><span>Careers</span></Link>
+                                <Link to="/careers" className=""><span>Careers</span></Link>
                               </li>
                             </ul>
                           </li>
                           <li className="menu-item has-sub-menu open_sub">
-                            <Link to="javascript:void(0)" className=""><span>Solutions</span></Link>
+                            <Link to="#" className=""><span>Solutions</span></Link>
                               <ul className="sub_menu">
                               <li className="menu-item menu-item-has-children has_sub open_sub"><h3><span>Service Delivery</span></h3>
                                 <ul className="sub_menu">
-                                  <li className="menu-item"><Link to="managed-services-uae-philippines.html" className=""><span>Managed Services in UAE &amp; Philippines</span></Link></li>
-                                  <li className="menu-item"><Link to="hosted-solutions.html" className=""><span>Hosted Solutions</span></Link></li>
-                                  <li className="menu-item"><Link to="disaster-recovery-business-continuity.html" className=""><span>Disaster Recovery &amp; Business Continuity</span></Link></li>
+                                  <li className="menu-item"><Link to="/managed_services_uae_philippines" className=""><span>Managed Services in UAE &amp; Philippines</span></Link></li>
+                                  <li className="menu-item"><Link to="/hosted_solutions" className=""><span>Hosted Solutions</span></Link></li>
+                                  <li className="menu-item"><Link to="/disaster_recovery_business_continuity" className=""><span>Disaster Recovery &amp; Business Continuity</span></Link></li>
                                 </ul>
                               </li>
                               <li className="menu-item has-sub-menu"><h3><span>Consultancy</span></h3>
                                 <ul className="sub_menu">
-                                  <li className="menu-item"><Link to="strategic-it-planning-architecture.html" className=""><span>Strategic IT Planning &amp; Architecture</span></Link></li>
-                                  <li className="menu-item"><Link to="network-end-point-security.html" className=""><span>Network &amp; End-Point Security Solutions</span></Link></li>
-                                  <li className="menu-item"><Link to="software-development.html" className=""><span>Software Development</span></Link></li>
+                                  <li className="menu-item"><Link to="/strategic_it_planning_architecture" className=""><span>Strategic IT Planning &amp; Architecture</span></Link></li>
+                                  <li className="menu-item"><Link to="/network_end_point_security" className=""><span>Network &amp; End-Point Security Solutions</span></Link></li>
+                                  <li className="menu-item"><Link to="/software_development" className=""><span>Software Development</span></Link></li>
                                 </ul>
                               </li>
                               <li className="menu-item menu-item-has-children has-sub-menu"><h3><span>Infrastructure</span></h3>
                                 <ul className="sub_menu">
-                                  <li className="menu-item"><Link to="it-office-setup-uae-philippines.html" className=""><span>IT Office Setup in UAE &amp; Philippines</span></Link></li>
-                                  <li className="menu-item"><Link to="smart-building-home-automation.html" className=""><span>Smart Building, Office &amp; Home Automation Solutions</span></Link></li>
-                                  <li className="menu-item"><Link to="structured-cabling-wireless.html" className=""><span>Structured Cabling &amp; Wireless</span></Link></li>
+                                  <li className="menu-item"><Link to="/it_office_setup_uae_philippines" className=""><span>IT Office Setup in UAE &amp; Philippines</span></Link></li>
+                                  <li className="menu-item"><Link to="/smart_building_home_automation" className=""><span>Smart Building, Office &amp; Home Automation Solutions</span></Link></li>
+                                  <li className="menu-item"><Link to="/structured_cabling_wireless" className=""><span>Structured Cabling &amp; Wireless</span></Link></li>
                                 </ul>
                               </li>
                               <li className="menu-item menu-item-has-children has-sub-menu"><h3><span>Communications</span></h3>
                                 <ul className="sub_menu">
-                                  <li className="menu-item"><Link to="unified-communications.html" className=""><span>Unified Communications</span></Link></li>
-                                  <li className="menu-item"><Link to="contact-centers.html" className=""><span>Contact Centers</span></Link></li>
-                                  <li className="menu-item"><Link to="hold-music-ivr-voice-recording.html" className=""><span>Hold Music &amp; IVR Voice Recording</span></Link></li>
+                                  <li className="menu-item"><Link to="/unified_communications" className=""><span>Unified Communications</span></Link></li>
+                                  <li className="menu-item"><Link to="/contact_centers" className=""><span>Contact Centers</span></Link></li>
+                                  <li className="menu-item"><Link to="/hold_music_ivr_voice_recording" className=""><span>Hold Music &amp; IVR Voice Recording</span></Link></li>
                                 </ul>
                             </li>
                           </ul>
                         </li>
-                        <li className="menu-item menu-item-has-children  has-sub-menu"><Link to="javascript:void(0)" className=""><span>Resources</span></Link>
+                        <li className="menu-item menu-item-has-children  has-sub-menu"><Link to="#" className=""><span>Resources</span></Link>
                           <ul className="sub_menu">
-                            <li className="menu-item"><Link to="articles.html" className=""><span>Articles</span></Link></li>
-                            <li className="menu-item"><Link to="open-source-software.html" className=""><span>Open Source Software</span></Link></li>
+                            <li className="menu-item"><Link to="/articles" className=""><span>Articles</span></Link></li>
+                            <li className="menu-item"><Link to="/open_source_software" className=""><span>Open Source Software</span></Link></li>
                           </ul>
                         </li>
-                        <li className="menu-item"><Link to="contact.html" className=""><span>Contact</span></Link></li>
+                        <li className="menu-item"><Link to="/contact" className=""><span>Contact</span></Link></li>
                       </ul>
                     </div>
 
@@ -258,7 +258,7 @@ return (
     </div>
 
     </header>
-    
+
     {children}
 
   <footer className="footer-section">
