567fa53797e28cbdcacf191b51f506e57f0eba27
nitish <developer.scorpio@gmail.com>
2020-02-24 18:09:45 +0530

    article pages

--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -1,29 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Img from "gatsby-image"
 export default ({ data }) => {
   const post = data.markdownRemark
-  console.log(post)
   return (
     <Layout>
-
       <div>
-      
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
-      )
-      )
-    }
     </Layout>
+  )
+}
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
-        frontmatter {
-          title
+      frontmatter {
+        title
       }
-    
     }
   }`
