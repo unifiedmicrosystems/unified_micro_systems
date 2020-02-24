2f7e4408df81564e05b06a6ae8f20feb401139f7
nitish <developer.scorpio@gmail.com>
2020-02-24 18:29:28 +0530

    article page

--- src/components/layout.js
+++ src/components/layout.js
@@ -258,39 +258,7 @@ return (
     </div>
 
     </header>
-      <div class="fullscreen_search_holder fade">
-    <div class="close_container">
-      <div class="container">
-        <div class="container_inner clearfix">
-            <div class="search_close_holder">
-              <div class="side_menu_button">
-                <a class="fullscreen_search_close" href="javascript:void(0)">
-                  <i class="qode_icon_font_awesome fa fa-times "></i>
-                </a>
-              </div>
-            </div>
-        </div>
-      </div>
-    </div>
-    <div class="fullscreen_search_table">
-      <div class="fullscreen_search_cell">
-        <div class="fullscreen_search_inner">
-          <form role="search" action="#" class="fullscreen_search_form" method="get">
-            <div class="form_holder">
-              <span class="search_label">Search:</span>
-              <div class="field_holder">
-                <input type="text" name="s" class="search_field" autocomplete="off">
-                <div class="line"></div>
-              </div>
-              <a class="qode_search_submit search_submit" href="javascript:void(0)">
-                <i class="qode_icon_font_awesome fa fa-search "></i>              </a>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-
+    
     {children}
 
   <footer className="footer-section">
