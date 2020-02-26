09bd2ac5204696bfa7147aee5d4003116a7e50be
devhardeep <44496083+devhardeep@users.noreply.github.com>
2020-02-26 13:55:15 +0530

    Update articles.js
    article page testimonials

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -59,6 +59,267 @@ export default ({ data }) => {
           </div>
           </div>
           </div>
+<div className="client-testimonials">
+    <div className="container">
+      <div className="testimonials-service">
+        <h3 className="aline-center">Testimonials</h3>
+      </div>
+      <div className="empty-space-50">&nbsp;</div>
+      <div id="client-testimonials-owl" className="owl-carousel owl-theme">
+      {(typeof window !== 'undefined') ? (
+      <OwlCarousel className="owl-carousel owl-theme" loop margin={10} items={3} autoplay={true} autoplayTimeout={2000} >
+        <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>The team will respond quickly and efficiently</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>Rather than a template based solution as my previous supplier suggested, I can pick up the phone and feel confident that the team will respond quickly and efficiently, providing me with a service and solution that suits my requirements.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Debbie&nbsp;Rogers (Owner,&nbsp;CoffeCakesAndRunning.com)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>I highly recommend their services</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>The support is on time and professional, and the advice we receive is unbiased and factual. I highly recommend their services – it’s a pleasure to work with such a professional IT provider.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Brian Ballinger (General Manager, Heidi Chef Solutions LLC)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>Comprehensive Support</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>In addition to comprehensive support, an office move and setup of infrastructure for two new businesses in other GCC countries. In all these endeavours Unified Microsystems have been of immense help and support.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Paul Loke (Regional Director, Edmundson Electrical)</p>
+              </div>
+            </div>
+          </div>
+        </div>   
+
+        <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>The team are simply awesome</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>I am impressed the professionalism, response time and quality of service. They take their time to explain how things work and make excellent recommendations.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Blagoy Savov (Clinic Director, Orthosports)</p>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>I highly recommend them</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>I appreciate the timely response whenever I need technical support or changes, they always respond with excellent knowledge. I am satisfied with their support and I highly recommend them.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Khurram Shehzad (Software Developer, Property Monitor)</p>
+              </div>
+            </div>
+          </div>
+        </div>  
+
+         <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>I highly recommend them</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>I appreciate the timely response whenever I need technical support or changes, they always respond with excellent knowledge. I am satisfied with their support and I highly recommend them.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Khurram Shehzad (Software Developer, Property Monitor)</p>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+
+         <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>I highly recommend them</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>I appreciate the timely response whenever I need technical support or changes, they always respond with excellent knowledge. I am satisfied with their support and I highly recommend them.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Khurram Shehzad (Software Developer, Property Monitor)</p>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+         <div className="item">
+          <div className="testimonial_content_inner">
+            <div className="testimonial_title_holder">
+              <h5>I highly recommend them</h5>
+            </div>
+            <div className="testimonial_rating_holder">
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+              <span className="testimonial_star_holder">
+                <i className="fa fa-star" aria-hidden="true"></i>
+              </span>
+            </div>
+            <div className="testimonial_text_holder">
+              <div className="testimonial_text_inner" >
+                <p>I appreciate the timely response whenever I need technical support or changes, they always respond with excellent knowledge. I am satisfied with their support and I highly recommend them.</p>
+                <p className="testimonial_author" style={{fontWeight: '500',color: '#be1522',fontSize: '13px'}}>- Khurram Shehzad (Software Developer, Property Monitor)</p>
+              </div>
+            </div>
+          </div>
+        </div> 
+        </OwlCarousel>
+        ) : null}
+        
+      </div>
+    </div>
+  </div>
     </Layout>
     )
 }
