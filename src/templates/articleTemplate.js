7b01b7c615f42779aedb6ddc9256fca73a72d3ce
devhardeep <44496083+devhardeep@users.noreply.github.com>
2020-02-26 12:05:04 +0530

    Update articleTemplate.js
    single article page image

--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Img from "gatsby-image"
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
+        <div className="blog-post-image">
+          <Img fluid={post.frontmatter.featuredImage.childImageSharp.fluid} className="attachment-full" alt="strategy" />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
