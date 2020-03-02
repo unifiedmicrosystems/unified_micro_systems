5e70b94b7202cc42261e522b43cdb2d8ee3d3a29
nitish <developer.scorpio@gmail.com>
2020-03-02 15:20:09 +0530

    links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link } from "'@reach/router'"
 import ReactDOM from "react-dom";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
