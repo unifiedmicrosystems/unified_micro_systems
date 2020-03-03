ecb0f90b994244f7a465230e99063824f9035cfd
nitish <developer.scorpio@gmail.com>
2020-03-03 11:32:12 +0530

    form validation

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -107,15 +107,15 @@ const contact = () => (
           <div className="empty-space-100">&nbsp;</div>
           <div className="contact-form-view">
               <Formik
-      initialValues={{ email: '', password: '' }}
+      initialValues={{ your-email: '', password: '' }}
       validate={values => {
         const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
+        if (!values.your-email) {
+          errors.your-email = 'Required';
         } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.your-email)
         ) {
-          errors.email = 'Invalid email address';
+          errors.your-email = 'Invalid email address';
         }
         return errors;
       }}
