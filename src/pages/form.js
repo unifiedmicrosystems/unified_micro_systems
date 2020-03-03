4fbfa5274715eeaf95f61179a4bdcc8252ee3f1d
nitish <developer.scorpio@gmail.com>
2020-03-03 16:38:08 +0530

    form test

--- src/pages/form.js
+++ src/pages/form.js
@@ -16,6 +16,21 @@ const Basic = () => (
         ) {
           errors.youremail = 'Invalid email address';
         }
+        if (!values.yourname) {
+          errors.yourname = 'Required';
+        } 
+        if (!values.yourmessage)
+        {
+          errors.yourmessage = 'Required';
+        }
+        if (!values.yourlastname)
+        {
+          errors.yourlastname = 'Required';
+        }
+        if (!values.yournumber)
+        {
+          errors.yournumber = 'Required';
+        }
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
@@ -29,17 +44,27 @@ const Basic = () => (
         <Form name="contact-form1" action="thank-you" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact-form1" />
             <input type="hidden" name="bot-field" />
-        <p><label> Your Email (required)<br />
-                <span className="wpcf7-form-control-wrap your-name">
-          <Field type="email" name="youremail" />
-
-          <ErrorMessage name="youremail" component="div" /></span></label></p>
-                <p><label> Your Name (required)<br />
-                <span className="wpcf7-form-control-wrap your-name"><Field type="text" name="yourname" size="40" className="" aria-required="true" aria-invalid="false" />                
-                <ErrorMessage name="yourname" component="div" /></span> </label></p>
-          <p><label> Your Message<br />
-            <span className="wpcf7-form-control-wrap your-message"><Field type="textarea" name="yourmessage" cols="40" rows="10" className="textarea" aria-invalid="false"></Field><ErrorMessage name="youremail" component="div" /></span> </label>
-          </p>
+        
+                <p><label> Your Name *<br />
+                  <span className="wpcf7-form-control-wrap your-name"><Field type="text" name="yourname" size="40" className="" aria-required="true" aria-invalid="false" />                
+                  <ErrorMessage name="yourname" component="div" /></span> </label>
+                </p>
+                <p><label>Last Name *<br />
+                  <span className="wpcf7-form-control-wrap your-email"><Field type="text" name="yourlastname" size="40" className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"/>
+                  <ErrorMessage name="yourlastname" component="div" /></span> </label>
+                </p>
+                <p><label> Your Email *<br />
+                  <span className="wpcf7-form-control-wrap your-name">
+                  <Field type="email" name="youremail" />
+                  <ErrorMessage name="youremail" component="div" /></span></label>
+                </p>
+                <p><label>Number *<br />
+                  <span className="wpcf7-form-control-wrap your-email"><Field type="Number" name="yournumber" size="40" className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"/>
+                  <ErrorMessage name="yournumber" component="div" /></span> </label>
+                </p>
+                <p><label> Your Message *<br />
+                  <span className="wpcf7-form-control-wrap your-message"><Field type="textarea" name="yourmessage" cols="40" rows="10" className="textarea" aria-invalid="false"></Field><ErrorMessage name="youremail" component="div" /></span> </label>
+                </p>
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
