5139b3342f081bbbd32aea12cae2a7c3925580ae
nitish <developer.scorpio@gmail.com>
2020-03-11 10:34:00 +0530

    menu toggle

--- src/components/NavbarLinks.js
+++ src/components/NavbarLinks.js
@@ -1,5 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+const NavLink = props => (
+  <Link
+    {...props}
+    getProps={({ isCurrent }) => {
+      // the object returned here is passed to the
+      // anchor element's props
+      return {
+        style: {
+          color: isCurrent ? "red" : "black"
+        }
+      };
+    }}
+  />
+);
 
 const NavbarLinks = () => {
   return (
